a796d340da83e0e97f5198c97d04f359623a6a10
Andrew Ivchenkov <and.ivchenkov@gmail.com>
2018-11-05 13:00:01 +0300

    Autocomplete query

---
+++ tests/Queries/AutocompleteQueryTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by Andrew Ivchenkov <and.ivchenkov@gmail.com>
+ * Date: 05.11.18
+ */
+
+namespace BookingCom\Tests\Queries;
+
+
+use PHPUnit\Framework\TestCase;
+
+class AutocompleteQueryTest extends TestCase
+{
+    public function testQuery(): void
+    {
+        $query = new \BookingCom\Queries\AutocompleteQuery('test', 'en');
+        $query->setAffiliateId(1)
+            ->withEndorsements()
+            ->withThemes()
+            ->withForecast()
+            ->withTimezones();
+
+        $this->assertEquals([
+            'text' => 'test',
+            'language' => 'en',
+            'affiliate_id' => 1,
+            'extras' => 'endorsements,themes,forecast,timezones',
+        ], $query->toArray());
+    }
+}
